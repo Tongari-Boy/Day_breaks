c7b95f3f2d21ab1d9943d96617169c40fb8ca374
Tongari-Boy <tongariboy@outlook.com>
2026-07-11 16:42:51 +0900

    城の体力バーUI処理

--- Assets/Scripts/Play/Castle/CastleUI.cs
+++ Assets/Scripts/Play/Castle/CastleUI.cs
@@ -1,11 +1,12 @@
-using UnityEngine;
+using UI;
 
 namespace Castle
 {
     /// <summary>
     /// 城関係のUIを管理するクラス
     /// </summary>
-    public class CastleUI : MonoBehaviour
+    public class CastleUI : HPBarUI
     {
+        // 城独自のUI演出をここに書く
     }
 }
--- Assets/Scripts/Play/DecoyFortress/DecoyFortressUI.cs
+++ Assets/Scripts/Play/DecoyFortress/DecoyFortressUI.cs
@@ -1,37 +1,12 @@
-using UnityEngine;
-using UnityEngine.UI;
+using UI;
 
 namespace DecoyFortress
 {
     /// <summary>
     /// 罠砦関係のUIを管理するクラス
     /// </summary>
-    public class DecoyFortressUI : MonoBehaviour
+    public class DecoyFortressUI : HPBarUI
     {
-        [SerializeField] private Slider hpSlider;
-
-        public void SetUp(int maxHP,int currentHP)
-        {
-            if (hpSlider == null) hpSlider = GetComponentInChildren<Slider>();
-
-            if (hpSlider != null)
-            {
-                hpSlider.maxValue = maxHP;
-                hpSlider.value = currentHP;
-            }
-        }
-
-        public void UpdateHP(int currentHP)
-        {
-            if(hpSlider != null)
-            {
-                hpSlider.value = currentHP;
-            }
-        }
-
-        public void SetActive(bool isActive)
-        {
-            gameObject.SetActive(isActive);
-        }
+        // 罠砦独自のUI処理を書く
     }
 }
