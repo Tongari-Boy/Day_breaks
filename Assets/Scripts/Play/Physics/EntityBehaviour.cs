cb3e08cb48f51b342632e5aed5424f0425f4316a
Senju8 <senju681020@outlook.com>
2026-07-15 00:30:55 +0900

    added physics manager

---
+++ Assets/Scripts/Play/Physics/EntityBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Physics
+{
+    /// <summary>
+    /// このクラスをGameObjectにアタッチするとPhysicsManagerに紐づけられる
+    /// </summary>
+    public class EntityBehaviour : MonoBehaviour
+    {
+        private readonly Entity entity = new(Shape.SQUARE);
+
+        /// <summary>
+        /// GameObjectの位置や速度を保持
+        /// </summary>
+        public Entity Entity
+        {
+            get { return this.entity; }
+        }
+
+        public void Start()
+        {
+            // このGameObjectとEntityを紐づける
+            PhysicsManager.INSTANCE.Register(this.gameObject, this.entity);
+        }
+
+        public void Update()
+        {
+            // GameObjectの座標を更新する
+            this.transform.position = this.entity.Position;
+        }
+
+        public void OnDestroy()
+        {
+            // このGameObjectとEntityの紐づけを解除
+            PhysicsManager.INSTANCE.Remove(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Play/Physics/PhysicsManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+
+namespace Physics
+{
+    /// <summary>
+    /// <para>GameObject同士の衝突を検知するための物理エンジン</para>
+    /// <para>インスタンスを取得するにはPhysicsManager.INSTANCEを参照する</para>
+    /// </summary>
+    public class PhysicsManager
+    {
+        public static readonly PhysicsManager INSTANCE = new();
+
+        private readonly Dictionary<GameObject, Entity> entities = new();
+
+        private PhysicsManager() { }
+
+        /// <summary>
+        /// GameObjectを物理空間に追加
+        /// </summary>
+        public void Register(GameObject gameObject, Entity entity)
+        {
+            this.entities.Add(gameObject, entity);
+        }
+
+        /// <summary>
+        /// GameObjectを物理空間から削除
+        /// </summary>
+        public void Remove(GameObject gameObject)
+        {
+            this.entities.Remove(gameObject);
+        }
+
+        /// <summary>
+        /// PhysicsManagerの初期化
+        /// </summary>
+        public void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// PhysicsManagerの更新
+        /// </summary>
+        public void OnUpdate()
+        {
+            this.Collide();
+            this.Update();
+        }
+
+        /// <summary>
+        /// 衝突を検知
+        /// </summary>
+        private void Collide()
+        {
+        }
+
+        /// <summary>
+        /// Entityの情報を更新
+        /// </summary>
+        private void Update()
+        {
+            foreach (Entity entity in this.entities.Values)
+            {
+                entity.Position += Time.deltaTime * entity.Velocity;
+                entity.Velocity -= Time.deltaTime * entity.Friction * entity.Velocity;
+            }
+        }
+    }
+}
